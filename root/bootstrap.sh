2de3d67dd4741669ed8fad1374039065c38d35f0
Asep Ready <savalunova@gmail.com>
2024-12-24 20:03:53 +0700

    secure

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -1,3 +1,5 @@
-openssl rand -base64 32 | tr -d '\n' > /root/rahasia.txt
-key=$(/root/rahasia.txt)
-sed -i -e "s/\$cfg\['blowfish_secret'\] = '';/\$cfg\['blowfish_secret'\] = \\sodium_hex2bin(\'$key\'');/g" /usr/local/www/phpMyAdmin/config.inc.php
+SECRET=$(openssl rand -base64 32)
+sed -i -e "s|\$cfg\['blowfish_secret'\] = .*;|\$cfg['blowfish_secret'] = \\sodium_hex2bin('$SECRET');|" /usr/local/www/phpMyAdmin/config.inc.php
+
+echo 'Verify .....'
+grep "blowfish_secret" /usr/local/www/phpMyAdmin/config.inc.php
