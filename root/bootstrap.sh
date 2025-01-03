d40a337bf67e3698ac177414e0a6a79974e408e1
Asep Ready <savalunova@gmail.com>
2025-01-04 04:45:15 +0700

    rewrite

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -3,3 +3,7 @@ sed -i -e "s|\$cfg\['blowfish_secret'\] = .*;|\$cfg['blowfish_secret'] = \\\sodi
 
 echo 'Verify .....'
 grep "blowfish_secret" /usr/local/www/phpMyAdmin/config.inc.php
+
+# Apache configuration PHP
+printf '<?php phpinfo(); ?>\n\n' > /usr/local/www/nginx/info.php
+rm /root/bootstrap.sh
