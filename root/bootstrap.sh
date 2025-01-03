a14b058c244d52699eb5f7d4f9cf48a8fc8e3311
Asep Ready <savalunova@gmail.com>
2025-01-04 04:50:26 +0700

    rewrite

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -5,5 +5,8 @@ echo 'Verify .....'
 grep "blowfish_secret" /usr/local/www/phpMyAdmin/config.inc.php
 
 # Apache configuration PHP
+sed -i '' 's|;date.timezone =|date.timezone ="Asia/Jakarta"|' /usr/local/etc/php.ini
+sed -i '' 's|mysqli.default_socket =|mysqli.default_socket =\/var\/run\/mysql\/mysql.sock|' /usr/local/etc/php.ini
+
 printf '<?php phpinfo(); ?>\n\n' > /usr/local/www/info.php
 rm /root/bootstrap.sh
