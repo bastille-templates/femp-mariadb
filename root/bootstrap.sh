5126e86bd0b9a8897a6af7c4c21e090efc681e0d
Asep Ready <savalunova@gmail.com>
2025-01-04 06:45:23 +0700

    add

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -4,7 +4,7 @@ sed -i -e "s|\$cfg\['blowfish_secret'\] = .*;|\$cfg['blowfish_secret'] = \\\sodi
 echo 'Verify .....'
 grep "blowfish_secret" /usr/local/www/phpMyAdmin/config.inc.php
 
-# Apache configuration PHP
+# Configuration PHP
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 sed -i '' 's|;date.timezone =|date.timezone ="Asia/Jakarta"|' /usr/local/etc/php.ini
 sed -i '' 's|mysqli.default_socket =|mysqli.default_socket =\/var\/run\/mysql\/mysql.sock|' /usr/local/etc/php.ini
